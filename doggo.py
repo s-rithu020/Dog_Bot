eb6193ce522963510666074ef3daad94ebf8f0ff
s-rithu020 <70939522+s-rithu020@users.noreply.github.com>
2020-09-10 21:20:25 +0530

    Create doggo.py

---
+++ doggo.py
@@ -0,0 +1,19 @@
+!pip install python-telegram-bot
+from telegram.ext import Updater, CommandHandler
+import requests #getting data from cloud
+
+def get_url():
+  contents = requests.get('https://random.dog/woof.json').json()
+  url = contents['url']
+  return url
+
+def dog(bot,update):
+  url = get_url()
+  chat_id = update.message.chat_id
+  bot.send_photo(chat_id,photo=url)
+
+u = Updater('1032331580:AAFmatXB9N_uu0bXT6oaXSsaGJ04Jg_mItw')
+dp = u.dispatcher
+dp.add_handler(CommandHandler('dog',dog))
+u.start_polling()
+u.idle()
